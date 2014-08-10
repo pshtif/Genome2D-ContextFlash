0c7add8aeea7afd21d1a01085f49b68f68812a1d
Peter "sHTiF" Stefcek <pshtif@gmail.com>
2014-08-10 22:46:39 +0200

    - FIXED stats no longer move when using custom cameras

--- src/com/genome2d/context/stats/GStats.hx
+++ src/com/genome2d/context/stats/GStats.hx
@@ -66,6 +66,9 @@ class GStats implements IStats
 
     public function render(p_context:IContext):Void {
         if (!visible) return;
+
+        p_context.setCamera(p_context.getDefaultCamera());
+
         var time:Int = untyped __global__["flash.utils.getTimer"]();
 
         if (time - 1000 > g2d_previousTime) {
