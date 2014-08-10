960ae03dbbf3fbda8fa1b6460cc247296ae135a0
Peter "sHTiF" Stefcek <pshtif@gmail.com>
2014-08-10 22:47:31 +0200

    - FIXED buttonDown and ctrlKey now have correct information - ADDED altKey and shiftKey

--- src/com/genome2d/signals/GMouseSignal.hx
+++ src/com/genome2d/signals/GMouseSignal.hx
@@ -11,10 +11,12 @@ package com.genome2d.signals;
 class GMouseSignal {
     public var x:Float;
     public var y:Float;
-    public var buttonDown:Bool;
-    public var ctrlDown:Bool;
+    public var buttonDown:Bool = false;
+    public var ctrlKey:Bool = false;
+    public var altKey:Bool = false;
+    public var shiftKey:Bool = false;
     public var type:String;
-    public var nativeCaptured:Bool;
+    public var nativeCaptured:Bool = false;
 
     public function new(p_type:String, p_x:Float, p_y:Float, p_nativeCaptured:Bool) {
         type = p_type;
