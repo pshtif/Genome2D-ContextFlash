f9a1e1c98f93871d477f21efafd1b488d715f33c
Peter @sHTiF Stefcek <pshtif@gmail.com>
2015-05-14 20:21:44 +0200

    added GOutlineFilter

--- src/com/genome2d/context/filters/GOutlineFilter.hx
+++ src/com/genome2d/context/filters/GOutlineFilter.hx
@@ -13,14 +13,13 @@ import flash.Vector;
 import com.genome2d.textures.GTexture;
 class GOutlineFilter extends GFilter
 {
-    public var blur:Float = 0;
+    public var size:Float = 0;
 
     public var red:Float = 0;
     public var green:Float = 0;
     public var blue:Float = 0;
-    public var alpha:Float = 1;
 
-    public function new(p_blur:Int, p_direction:Int) {
+    public function new(p_size:Int) {
         super();
 
         overrideFragmentShader = true;
@@ -29,19 +28,19 @@ class GOutlineFilter extends GFilter
             "tex ft0, v0, fs0 <2d,linear,mipnone,clamp>     \n" +
             "mul ft0.w, ft0.w, fc2.x                        \n" +
 
-            "sub ft1.xy, v0.x, fc1.x                        \n" +
+            "sub ft1.xy, v0.xy, fc1.xy                      \n" +
             "tex ft2, ft1.xy, fs0 <2d,linear,mipnone,clamp> \n" +
             "sub ft0.w, ft0.w, ft2.w                        \n" +
 
-            "add ft1.xy, v0.x, fc1.x                        \n" +
+            "add ft1.xy, v0.xy, fc1.xy                      \n" +
             "tex ft2, ft1.xy, fs0 <2d,linear,mipnone,clamp> \n" +
             "sub ft0.w, ft0.w, ft2.w                        \n" +
 
-            "sub ft1.xy, v0.y, fc1.y                        \n" +
+            "sub ft1.xy, v0.xy, fc1.zw                      \n" +
             "tex ft2, ft1.xy, fs0 <2d,linear,mipnone,clamp> \n" +
             "sub ft0.w, ft0.w, ft2.w                        \n" +
 
-            "add ft1.xy, v0.y, fc1.y                        \n" +
+            "add ft1.xy, v0.xy, fc1.zw                      \n" +
             "tex ft2, ft1.xy, fs0 <2d,linear,mipnone,clamp> \n" +
             "sub ft0.w, ft0.w, ft2.w                        \n" +
 
@@ -50,18 +49,17 @@ class GOutlineFilter extends GFilter
 
         fragmentConstants = Vector.ofArray([0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0.0]);
 
-        blur = p_blur;
+        size = p_size;
     }
 
     override public function bind(p_context:IGContext, p_texture:GTexture):Void {
         // We do invalidation each bind as the textures parameters are crucial for constants
-        fragmentConstants[0] = 1 / p_texture.gpuWidth;// * 1.3846153846 * blur * .5;
-        fragmentConstants[1] = 1 / p_texture.gpuHeight;// * 1.3846153846 * blur * .5;
+        fragmentConstants[0] = size / p_texture.gpuWidth;
+        fragmentConstants[3] = size / p_texture.gpuHeight;
 
         fragmentConstants[8] = red;
         fragmentConstants[9] = green;
         fragmentConstants[10] = blue;
-        fragmentConstants[11] = alpha;
 
         super.bind(p_context, p_texture);
     }
