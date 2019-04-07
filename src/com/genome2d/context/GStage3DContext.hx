77f707c62aca228d686afb23df9dcd6193878660
sHTiF <pshtif@gmail.com>
2019-04-07 19:38:26 +0200

    BREAKING removed default parameters for performance

--- src/com/genome2d/context/GStage3DContext.hx
+++ src/com/genome2d/context/GStage3DContext.hx
@@ -613,7 +613,7 @@ class GStage3DContext implements IGDebuggableInternal implements IGFocusable
 
        @param p_texture textures instance used to drawing
      */
-	inline public function draw(p_texture:GTexture, p_blendMode:GBlendMode, p_x:Float, p_y:Float, p_scaleX:Float = 1, p_scaleY:Float = 1, p_rotation:Float = 0, p_red:Float = 1, p_green:Float = 1, p_blue:Float = 1, p_alpha:Float = 1, p_filter:GFilter = null):Void {
+	inline public function draw(p_texture:GTexture, p_blendMode:GBlendMode, p_x:Float, p_y:Float, p_scaleX:Float = 1, p_scaleY:Float = 1, p_rotation:Float, p_red:Float, p_green:Float, p_blue:Float, p_alpha:Float, p_filter:GFilter):Void {
 		if (p_alpha != 0 && p_texture.nativeTexture != null) {
             setBlendMode(p_blendMode, p_texture.premultiplied);
 			setRenderer(g2d_quadTextureShaderRenderer);
@@ -626,7 +626,7 @@ class GStage3DContext implements IGDebuggableInternal implements IGFocusable
 
        @param p_texture textures to be drawn
      */
-    inline public function drawSource(p_texture:GTexture, p_blendMode:GBlendMode, p_sourceX:Float, p_sourceY:Float, p_sourceWidth:Float, p_sourceHeight:Float, p_sourcePivotX:Float, p_sourcePivotY:Float, p_x:Float, p_y:Float, p_scaleX:Float = 1, p_scaleY:Float = 1, p_rotation:Float = 0, p_red:Float = 1, p_green:Float = 1, p_blue:Float = 1, p_alpha:Float = 1, p_filter:GFilter = null):Void {
+    inline public function drawSource(p_texture:GTexture, p_blendMode:GBlendMode, p_sourceX:Float, p_sourceY:Float, p_sourceWidth:Float, p_sourceHeight:Float, p_sourcePivotX:Float, p_sourcePivotY:Float, p_x:Float, p_y:Float, p_scaleX:Float, p_scaleY:Float, p_rotation:Float, p_red:Float, p_green:Float, p_blue:Float, p_alpha:Float, p_filter:GFilter):Void {
         if (p_alpha != 0 && p_texture.nativeTexture != null) {
             setBlendMode(p_blendMode, p_texture.premultiplied);
             setRenderer(g2d_quadTextureShaderRenderer);
@@ -640,7 +640,7 @@ class GStage3DContext implements IGDebuggableInternal implements IGFocusable
 
        @param p_texture textures to be drawn
      */
-    inline public function drawMatrix(p_texture:GTexture, p_blendMode:GBlendMode, p_a:Float, p_b:Float, p_c:Float, p_d:Float, p_tx:Float, p_ty:Float, p_red:Float = 1, p_green:Float = 1, p_blue:Float = 1, p_alpha:Float=1, p_filter:GFilter = null):Void {
+    inline public function drawMatrix(p_texture:GTexture, p_blendMode:GBlendMode, p_a:Float, p_b:Float, p_c:Float, p_d:Float, p_tx:Float, p_ty:Float, p_red:Float, p_green:Float, p_blue:Float, p_alpha:Float, p_filter:GFilter):Void {
         if (p_alpha != 0 && p_texture.nativeTexture != null) {
             setBlendMode(p_blendMode, p_texture.premultiplied);
             setRenderer(g2d_matrixQuadTextureShaderRenderer);
@@ -653,7 +653,7 @@ class GStage3DContext implements IGDebuggableInternal implements IGFocusable
 
        @param p_texture textures to be drawn
      */
-    inline public function drawMatrixSource(p_texture:GTexture, p_blendMode:GBlendMode, p_sourceX:Float, p_sourceY:Float, p_sourceWidth:Float, p_sourceHeight:Float, p_a:Float, p_b:Float, p_c:Float, p_d:Float, p_tx:Float, p_ty:Float, p_red:Float = 1, p_green:Float = 1, p_blue:Float = 1, p_alpha:Float=1, p_filter:GFilter = null):Void {
+    inline public function drawMatrixSource(p_texture:GTexture, p_blendMode:GBlendMode, p_sourceX:Float, p_sourceY:Float, p_sourceWidth:Float, p_sourceHeight:Float, p_a:Float, p_b:Float, p_c:Float, p_d:Float, p_tx:Float, p_ty:Float, p_red:Float, p_green:Float, p_blue:Float, p_alpha:Float, p_filter:GFilter):Void {
         if (p_alpha != 0 && p_texture.nativeTexture != null) {
             setBlendMode(p_blendMode, p_texture.premultiplied);
             setRenderer(g2d_matrixQuadTextureShaderRenderer);
@@ -674,7 +674,7 @@ class GStage3DContext implements IGDebuggableInternal implements IGFocusable
         @param p_scaleY y scale
         @param p_rotation rotation
      */
-    inline public function drawPoly(p_texture:GTexture, p_blendMode:GBlendMode, p_vertices:Array<Float>, p_uvs:Array<Float>, p_x:Float, p_y:Float, p_scaleX:Float = 1, p_scaleY:Float = 1, p_rotation:Float = 0, p_red:Float = 1, p_green:Float = 1, p_blue:Float = 1, p_alpha:Float = 1, p_filter:GFilter = null):Void {
+    inline public function drawPoly(p_texture:GTexture, p_blendMode:GBlendMode, p_vertices:Array<Float>, p_uvs:Array<Float>, p_x:Float, p_y:Float, p_scaleX:Float, p_scaleY:Float, p_rotation:Float, p_red:Float, p_green:Float, p_blue:Float, p_alpha:Float, p_filter:GFilter):Void {
         if (p_alpha != 0 && p_texture.nativeTexture != null) {
             setBlendMode(p_blendMode, p_texture.premultiplied);
             setRenderer(g2d_triangleTextureBufferCPURenderer);
